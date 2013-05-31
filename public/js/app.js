4369cb25fe6f75d0b8a7bb36c97f332dbc776bff
Horia Dragomir <horia@hdragomir.com>
2013-05-31 17:43:08 +0200

    we have a basic crud app

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 (function () {
   var H = Handlebars;
 
-  function WHEN() {
+  function WHEN(node) {
+    this.node = node;
     this.data = [];
     this.templates = {
-      "entry": Handlebars.compile(document.getElementById('template-entry').innerHTML)
+      "entry": H.compile(document.getElementById('template-entry').innerHTML),
+      "form": H.compile(document.getElementById('template-add-entry').innerHTML)
     };
   }
 
@@ -17,9 +19,13 @@
     for (when in data) { if (data.hasOwnProperty(when)) {
       this.data.push({
         when: when,
-        actions: data[when].actions
+        actions: data[when].actions,
+        ts: data[when].ts
       });
     } }
+    this.data.sort(function (a, b) {
+      return b.ts - a.ts;
+    });
     return this.data;
   };
 
@@ -27,7 +33,6 @@
     var save = {};
     this.data.forEach(function (when) {
       save[when.when] = when;
-      delete save[when.when].when;
     });
     localStorage.when = JSON.stringify(save);
   };
@@ -36,13 +41,35 @@
     return this.data.map(this.templates.entry).join("");
   };
 
-  document.addEventListener('DOMContentLoaded', function (e) {
-    var w = new WHEN(),
-      form = document.getElementById("add-entry"),
-      whenList = document.querySelector('.when-list');
-    w.load();
-    whenList.innerHTML = w.render();
-    form.addEventListener('keydown', function (e) {
+  WHEN.prototype.get = function (id) {
+    return this.data.filter(function (when) {
+      return when.when === id;
+    })[0];
+  };
+
+  WHEN.prototype.put = function (values) {
+    for (var i = this.data.length; i -- > 0; ) {
+      if (this.data[i].when === values.when) {
+        this.data[i] = values;
+        return this.data;
+      }
+    }
+    this.data.unshift(values);
+    return this.data;
+  };
+
+  WHEN.prototype.remove = function (id) {
+    return this.data = this.data.filter(function (when) {
+      return when.when !== id;
+    });
+  };
+
+  WHEN.prototype.paint = function () {
+    this.node.innerHTML = this.render();
+  };
+
+  WHEN.Helper = {
+    formKeyHandler: function (e) {
       if (e.which === 13 && e.target.name === "actions[]") {
         e.preventDefault();
         e.stopPropagation();
@@ -53,30 +80,105 @@
         e.target.parentNode.parentNode.appendChild(clone);
         input.focus();
       }
-    }, false);
+    },
+    addEscHandler: function (node, fn) {
+      node.addEventListener('keyup', function (e) {
+        if (e.which === 27) {
+          fn.call(node, e);
+        }
+      }, false);
+      return node;
+    },
+    extractFormValues: function (e) {
+      var values = {
+          when: null,
+          actions: [],
+          ts: Date.now()
+        },
+        form = e.target,
+        carry = ["when", "ts"];
+      [].forEach.call(form, function (input) {
+        var value = input.value.trim();
+        if (value) {
+          if (carry.indexOf(input.name) !== -1) {
+            values[input.name] = input.value.trim();
+          } else if (input.name === "actions[]") {
+            values.actions.push(input.value.trim());
+          }
+        }
+      });
+      return {values: values, form: form};
+    },
+    getParentWhenNode: function (node) {
+      do {
+        if (node.classList.contains('editable')) {
+          break;
+        } else {
+          node = node.parentNode || null;
+        }
+      } while(node);
+      return node;
+    }
+  };
 
+
+  document.addEventListener('DOMContentLoaded', function (e) {
+    var w,
+      form = document.getElementById("add-entry"),
+      whenList = document.querySelector('.when-list');
+    w = new WHEN(whenList);
+    w.load();
+    w.paint();
+    form.addEventListener('keydown', WHEN.Helper.formKeyHandler, false);
     form.addEventListener('submit', function (e) {
       if (!e.defaultPrevented) {
         e.preventDefault();
-        var values = {when: null, actions: []}, form = e.target;
-        [].forEach.call(form, function (input) {
-          var value = input.value.trim();
-          if (value) {
-            if (input.name === "when") {
-              values.when = input.value.trim();
-            } else if (input.name === "actions[]") {
-              values.actions.push(input.value.trim());
-            }
-          }
-        });
-        w.data.unshift(values);
+        var formAndValues = WHEN.Helper.extractFormValues(e),
+          form = formAndValues.form;
         [].forEach.call(form.querySelectorAll('.action'), function (el, i) {
           i && el.parentNode.removeChild(el);
         });
         form.reset();
-        whenList.innerHTML = w.render();
+        w.put(formAndValues.values);
+        w.paint();
         w.save();
       }
     }, false);
+
+    whenList.addEventListener('click', function (e) {
+      if (e.shiftKey) {
+        var whenNode = WHEN.Helper.getParentWhenNode(e.srcElement);
+        if (whenNode) {
+          var id = whenNode.dataset.id;
+          whenNode.innerHTML = w.templates.form(w.get(id));
+          whenNode.querySelector('[name=when]').focus();
+          whenNode.addEventListener('keydown', WHEN.Helper.formKeyHandler, false);
+          whenNode.addEventListener('submit', function (e) {
+            if (!e.defaultPrevented) {
+              e.preventDefault();
+              var formAndValues = WHEN.Helper.extractFormValues(e),
+                form = formAndValues.form,
+                values = formAndValues.values;
+              w.put(values);
+              whenList.innerHTML = w.render();
+              w.save();
+            }
+          }, false);
+          whenNode.addEventListener('click', function (e) {
+            if (e.srcElement.classList.contains('delete')) {
+              if (confirm("really?")) {
+                w.remove(id);
+                w.paint();
+                w.save();
+              }
+            }
+          }, false);
+          WHEN.Helper.addEscHandler(whenNode, function (e) {
+            w.paint();
+          });
+        }
+      }
+    }, false);
+    form.innerHTML = w.templates.form({});
   }, false);
 }());
